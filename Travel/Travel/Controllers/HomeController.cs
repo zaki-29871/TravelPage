6d89108c3a48f80f22a98feddfb84e428abdf8a8
Jack <z2589906@gmail.com>
2016-11-25 19:24:01 +0800

    first commit

---
+++ Travel/Travel/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Travel.Models;
+
+namespace Travel.Controllers
+{
+    public class HomeController : Controller
+    {
+        private SelabTravelEntities db = new SelabTravelEntities();
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Member()
+        {
+            return View(from member in db.Member select member);
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
