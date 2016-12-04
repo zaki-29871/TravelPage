cddb71d3f0c1f9dfb3b881544ef5cf28abbf7c89
Jack <z2589906@gmail.com>
2016-12-04 21:50:04 +0800

    新增Ajax教學

---
+++ AjaxTutorial/AjaxTutorial/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AjaxTutorial.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            string getName = Request.QueryString["name"];
+            string postName = Request.Form["name"];
+
+            if (getName != null)
+                return View((object)(getName + " GET return"));
+
+            else if (postName != null)
+                return View((object)(postName + " POST return"));
+
+            else return View((object)"error");
+        }
+
+        /*[HttpPost]
+        public ActionResult Index(string name)
+        {
+            return View((object)(name + " POST return"));
+        }*/
+
+        [HttpPost]
+        public string Ajax(string name)
+        {
+            return name + " Ajax return";
+        }
+    }
+}
